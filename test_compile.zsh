06a9ad2be1c487da8c1619dfa172d06558f25690
Andy Palmer <andy@andypalmer.com>
2019-02-10 12:37:30 +1100

    We enable all warnings for our own code, explain the warnings we disable for CPPUTest

--- test_compile.zsh
+++ test_compile.zsh
@@ -1,13 +1,18 @@
 #!/usr/local/bin/zsh
 mkdir -p build/objs/{src,tests}
 for f in src/**/*.cpp; do
-  c++ -g -Wall -Weverything -Werror -Wswitch-default -Wswitch-enum -Wno-unused-parameter -Wno-c99-extensions -Wno-keyword-macro -Wno-reserved-id-macro -Wno-c++11-long-long -Weverything -Wno-disabled-macro-expansion -Wno-padded -Wno-global-constructors -Wno-exit-time-destructors -Wno-weak-vtables -Wno-old-style-cast \
+  c++ -g -Wall -Wextra -Weverything -Werror \
     -Iinclude/geometry \
     -c -MMD -MP \
     -o build/objs/${f:r}.o ${f}
 done
+
+CPPUTEST_MEMORY_LEAK_WARNING_DISABLE=(-Wno-c++11-long-long -Wno-keyword-macro -Wno-reserved-id-macro)
+CPPUTEST_FRAMEWORK_WARNING_DISABLE=(-Wno-old-style-cast -Wno-disabled-macro-expansion -Wno-weak-vtables -Wno-global-constructors -Wno-exit-time-destructors -Wno-padded)
 for f in tests/**/*.cpp; do
-  c++ -g -Wall -Weverything -Werror -Wswitch-default -Wswitch-enum -Wno-unused-parameter -Wno-c99-extensions -Wno-keyword-macro -Wno-reserved-id-macro -Wno-c++11-long-long -Wno-disabled-macro-expansion -Wno-padded -Wno-global-constructors -Wno-exit-time-destructors -Wno-weak-vtables -Wno-old-style-cast \
+  c++ -g -Wall -Wextra -Weverything -Werror \
+    $CPPUTEST_MEMORY_LEAK_WARNING_DISABLE \
+    $CPPUTEST_FRAMEWORK_WARNING_DISABLE \
     -DCPPUTEST_COMPILATION \
     -include /usr/local/Cellar/cpputest/3.8/include/CppUTest/MemoryLeakDetectorNewMacros.h \
     -include /usr/local/Cellar/cpputest/3.8/include/CppUTest/MemoryLeakDetectorMallocMacros.h \
